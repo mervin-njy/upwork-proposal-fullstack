6827efec862edceb1c7918a8912a4b45f820d205
mervin-njy <mervin.njy@outlook.com>
2024-03-06 13:53:11 +0800

    added more validation for form fields + styling changes

--- coding-challenge-3/react-ts-login/src/App.tsx
+++ coding-challenge-3/react-ts-login/src/App.tsx
@@ -19,7 +19,11 @@ function App() {
   } = useForm<FormFields>();
 
   // function to handle the form submission notification => only called once the form is valid
-  const onSubmit: SubmitHandler<FormFields> = (data) => {
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    // simulate a server action to get the user from the database
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // log the form data
     console.log(data);
   };
 
@@ -28,47 +32,69 @@ function App() {
     // form element that uses the react-hook-form hook to handle the form submission callback
     // handleSubmit function prevents the default form submission, checks that the FormFields type is valid and calls the onSubmit function
     <form
-      className="border-1 w-96 rounded-2xl border-purpleAccent px-8 py-8"
+      className="w-96 rounded-2xl border-1 border-purpleAccent px-8 py-8"
       onSubmit={handleSubmit(onSubmit)}
     >
       <div className="mb-2 h-16">
-        {/* form input field: email including validation */}
+        {/* INPUT 1: email including validation */}
         <input
           type="email"
           placeholder="Email"
-          className="w-full rounded-lg border-2 border-main8 bg-main9 px-3 py-2 text-main transition duration-300 ease-in-out hover:border-purpleAccent hover:bg-main8 hover:text-purpleAccent focus:outline-none focus:ring focus-visible:ring-purpleAccent focus-visible:ring-opacity-75"
-          {...register("email", { required: true })}
+          autoComplete="email"
+          className="w-full rounded-lg border-2 border-main8 bg-main9 px-3 py-2 text-main transition duration-300 ease-in-out hover:border-main3 hover:bg-main8 hover:text-main3 focus:border-purpleAccent focus:text-purpleAccent focus:outline-none"
+          {...register("email", {
+            required: "Email field is required.", // returns string as error message if empty
+            validate: (value) => {
+              if (!value.includes("@")) return "Email must include '@' symbol";
+              if (!value.includes(".")) return "Email must include '.' symbol";
+              if (
+                !value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+              )
+                // validate email format ___@___.___ using regex
+                return "Please enter a valid email address.";
+              return true;
+            },
+          })}
         />
+        {/* ERROR MESSAGE: email validation */}
         {errors.email && (
           <span className="mt-1 flex px-2 text-sm text-redAccent">
-            This field is required!
+            {errors.email.message}
           </span>
         )}
       </div>
 
       <div className="mb-2 h-16">
-        {/* form input field: password including validation */}
+        {/* INPUT 2: password including validation */}
         <input
           type="password"
           placeholder="Password"
-          className="w-full rounded-lg border-2 border-main8 bg-main9 px-3 py-2 text-main transition duration-300 ease-in-out hover:border-purpleAccent hover:bg-main8 hover:text-purpleAccent focus:outline-none focus:ring focus-visible:ring-purpleAccent focus-visible:ring-opacity-75"
-          {...register("password", { required: true })}
+          autoComplete="off"
+          className="w-full rounded-lg border-2 border-main8 bg-main9 px-3 py-2 text-main transition duration-300 ease-in-out hover:border-main3 hover:bg-main8 hover:text-main3 focus:border-purpleAccent focus:text-purpleAccent focus:outline-none"
+          {...register("password", {
+            required: "Password field is required.", // returns string as error message if empty
+            validate: (value) =>
+              value.length >= 8 ||
+              "Password must be at least 8 characters long", // validate length of password
+          })}
         />
+        {/* ERROR MESSAGE: password validation */}
         {errors.password && (
           <span className="mt-1 flex px-2 text-sm text-redAccent">
-            This field is required!
+            {errors.password.message}
           </span>
         )}
       </div>
 
-      {/* form submit button */}
+      {/* LOGIN button */}
       <div className="flex justify-end">
         <button
           type="submit"
-          className="rounded-lg bg-purpleMain px-4 py-2 tracking-wide text-main transition duration-500 ease-in-out hover:bg-purpleAccent hover:text-mainDarkest"
+          className="rounded-lg bg-purpleMain px-4 py-2 tracking-wide text-main transition duration-500 ease-in-out hover:bg-purpleAccent hover:text-mainDarkest focus:outline-none"
         >
           Login
         </button>
+        {/* ERROR MESSAGE: failed server action to getUser - user doesn't exist */}
       </div>
     </form>
   );
